eb65ffc6cb2f1434864dfe606d2ef3a45c7b6889
Franco Cappitta <franco84@gmail.com>
2016-11-01 03:07:16 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,8 @@
 def key_for_min_value(name_hash)
   keyy = nil
-  valuee = 0
+  valuee = nil
   name_hash.each do |key, value|
-    if valuee == 0 || value < valuee
+    if valuee == nil || value < valuee
       valuee = value
       keyy = key
     end
